542ccf39b4528e236c7b613d0b7dcbf43c1d14da
AJOTKA <agata.kowalewska@gmail.com>
2018-01-12 17:01:35 +0100

    create GUI for chrome extension

---
+++ popup.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2018 AJOTKA. All rights reserved.
+
+function click(e) {
+  /* TODO: execute damnfinder.js wit dynamic vars */
+  chrome.tabs.executeScript(null,
+      {code:
+      	"document.body.style.backgroundColor='" + highlight + "'"
+      });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	/* TODO: load the variables */
+});
+
+
+/* events with inputs & textarea */
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+/* change tabs */
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
